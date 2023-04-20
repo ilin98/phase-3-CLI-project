ad90edfa95b3bee6d006e0ff8df73cbd708fa8e6
Isaac <isaac.lin.pnc@gmail.com>
2023-04-20 14:29:03 -0400

    create Teams table

--- lib/models.py
+++ lib/models.py
@@ -5,3 +5,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class Teams(Base):
+    __tablename__ = 'teams'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    conference = Column(String())
+    division = Column(String())
+    wins = Column(Integer())
+    losses = Column(Integer())
+
+    def __repr__(self):
+        return f'{self.name}, {self.conference}, {self.division} Division, ' + \
+            f'record = {self.wins} - {self.losses}'

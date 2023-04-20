8d7fe7697f2308fef453bab57a2679056642aaee
Isaac <isaac.lin.pnc@gmail.com>
2023-04-20 15:28:13 -0400

    created players and coaches tables;

--- lib/models.py
+++ lib/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import func
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
@@ -16,6 +16,35 @@ class Teams(Base):
     wins = Column(Integer())
     losses = Column(Integer())
 
+    players = relationship('Players', backref=backref('teams'))
+    coaches = relationship('Coaches', backref=backref('teams'))
+
     def __repr__(self):
         return f'{self.name}, {self.conference}, {self.division} Division, ' + \
             f'record = {self.wins} - {self.losses}'
+
+class Players(Base):
+    __tablename__ = 'players'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    age = Column(Integer())
+    position = Column(String())
+
+    team_id = Column(Integer(), ForeignKey('teams.id'))
+
+    def __repr__(self):
+        return f'{self.name}, {self.position}, ' + \
+            f'age: {self.age}'
+
+class Coaches(Base):
+    __tablename__ = 'coaches'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    age = Column(Integer())
+
+    team_id = Column(Integer(), ForeignKey('teams.id'))
+
+    def __repr__(self):
+        return f'{self.name}, {self.age}'
